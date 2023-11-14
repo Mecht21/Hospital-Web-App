505a5c9c3bb4c0c8671e838312b1f5fea35971e8
Daniel <daniel.serrano0119@gmail.com>
2023-11-13 18:25:54 -0600

    Generacion del Modelo con Entity FrameWork

---
+++ Model/Context/DBConnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Models
+{
+    public partial class DBConnection : DbContext
+    {
+        private DBConnection(string stringConnection)
+            : base(stringConnection)
+        { 
+            this.Configuration.LazyLoadingEnabled = true;
+            this.Configuration.ProxyCreationEnabled = true;
+            this.Database.CommandTimeout = 900;
+        }
+
+        public static DBConnection Create()
+        {
+            return new DBConnection("name=DbConnection");
+        }
+    }
+}
